9bee79936909c3d770a93504980dbc96085653ed
Shaurya <142096007+shauryay7@users.noreply.github.com>
2025-04-12 00:28:36 +0530

    update

--- app/src/components/Footer/Footer.jsx
+++ app/src/components/Footer/Footer.jsx
@@ -4,28 +4,6 @@ import "bulma/css/bulma.min.css";
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="content has-text-centered">
-        <p>
-          <strong>Built</strong> by{" "}
-          <a href="https://tonyisern.com" target="_blank">
-            Tony Isern
-          </a>
-          ,{" "}
-          <a href="https://github.com/abdoul61" target="_blank">
-            Souley Abdoul Aziz
-          </a>
-          , and{" "}
-          <a href="https://github.com/elizaclamor" target="_blank">
-            Eliza Clamor
-          </a>
-          . The source code is on
-          <a href="https://github.com/OptimisticTrousers/cooking-recipe-portal">
-            {" "}
-            GitHub
-          </a>
-          .
-        </p>
-      </div>
     </footer>
   );
 };
--- server/data_base.js
+++ server/data_base.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const dotenv = require('dotenv'); 
 dotenv.config();
 const mysql_connection = mysql.createConnection({
@@ -6,7 +6,7 @@ const mysql_connection = mysql.createConnection({
 	user:process.env.DB_USER,
 	password:process.env.DB_PASSWORD,
 	database:process.env.DB,
-	multipleStatement:true
+	multipleStatements:true
 
 });
 
